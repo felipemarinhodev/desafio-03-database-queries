4bd83d072a5a5f26526f3eed23e79217d48722be
Felipe Marinho <felipmarinho@gmail.com>
2021-06-13 06:16:48 -0300

    ensure to find user with games list by user's ID

--- src/modules/users/repositories/implementations/UsersRepository.ts
+++ src/modules/users/repositories/implementations/UsersRepository.ts
@@ -14,7 +14,14 @@ export class UsersRepository implements IUsersRepository {
   async findUserWithGamesById({
     user_id,
   }: IFindUserWithGamesDTO): Promise<User> {
-    // Complete usando ORM
+    const user = await this.repository.findOne({
+      relations: ['games'],
+      where:{id: user_id}
+    })
+    if (!user) {
+      throw new Error("User not found.");
+    }
+    return user;
   }
 
   async findAllUsersOrderedByFirstName(): Promise<User[]> {
